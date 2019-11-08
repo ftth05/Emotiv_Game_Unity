17d29f7b4f5ffbaa1611c9f45107e3ed3e5ed1b4
Fettah <ftth05@gmail.com>
2019-11-08 00:43:07 -0600

    ContactQuality Added to Puzzle1 but needs some changes for rotations
    ContactQuality Added to Puzzle1 but needs some changes for rotations

---
+++ SDK-3.3.3/examples_basic/Unity/Puzzle1/Assets/Scripts/TouchRotate.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+
+{
+
+    /*
+    public static int action = 0;
+   //public static float speed = 7.0f / 8.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (action == 0)
+        {
+            if (!GameControl.youWin)
+                transform.Rotate(0f, 0f, 90f);
+            else
+            {
+                transform.localPosition = new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            //if (!GameControl.youWin)
+                transform.Rotate(0f, 0f, 0f);
+        }
+    }
+    */
+
+    public static int action = 0;
+
+
+    private void OnMouseDown()
+    {
+        if (action == 0) // simdilik push ile calisacak.
+        {
+            if (!GameControl.youWin)
+                transform.Rotate(0f, 0f, 90f);
+        }
+
+        else
+        {
+
+             void OnMouseEnter()
+            {
+
+                transform.localScale += new Vector3(-0.15f, -0.15f, 0);
+            }
+             void OnMouseExit()
+            {
+                transform.localScale += new Vector3(0.15f, 0.15f, 0);
+            }
+        }
+
+    }
+
+}
