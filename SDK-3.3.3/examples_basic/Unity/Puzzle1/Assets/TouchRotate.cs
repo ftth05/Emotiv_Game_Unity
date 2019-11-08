e1d1220df79c544aee8cdded0112c80dded1db9b
Fettah <ftth05@gmail.com>
2019-11-07 23:08:17 -0600

    SDK 3.3.3 Mac Puzzle1 without Emotiv
    SDK 3.3.3 Mac Puzzle1 without Emotiv

---
+++ SDK-3.3.3/examples_basic/Unity/Puzzle1/Assets/TouchRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+       
+        transform.localScale += new Vector3(-0.15f, -0.15f,0);
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale += new Vector3(0.15f, 0.15f, 0);
+    }
+    private void OnMouseDown()
+    {
+        if (!GameControl.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
+}
